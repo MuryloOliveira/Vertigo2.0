abb04dd298c55693f32fa1f91da5d4e8d0531861
Murylo <murylooliveira2010@gmail.com>
2023-06-13 18:33:47 -0300

    Commit final

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { Logo } from './components/logo'
-import { Header } from './components/Header'
-import { Formulario } from './components/Formulario'
-import fundoRoxo from './images/fundo-roxo.png'
-import './App.css'
+import React from 'react';
+import Logo from './components/logo';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import fundoRoxo from './images/fundo-roxo.png';
+import './App.css';
 
-export function App() {
-    return (
-        <section>
-            <div className="tela">
-                <Logo />
-                <Header />
-                <Formulario />
-            </div>
-            <div>
-                <img src={fundoRoxo} alt="imagem com fundo roxo" className="roxa" />
-            </div>
-        </section>
-    )
-}
+const App = () => (
+  <section>
+    <div className="tela">
+      <Logo />
+      <Header />
+      <Formulario />
+    </div>
+    <div>
+      <img src={fundoRoxo} alt="imagem com fundo roxo" className="roxa" />
+    </div>
+  </section>
+);
+
+export default App;
--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,10 +1,12 @@
-import logo from '../images/logo.png'
-import '../Logo.css'
+import logo from '../images/logo.png';
+import '../Logo.css';
 
-export function Logo() {
-    return (
-        <div className="logo">
-            <img src={logo} alt="logo" />
-        </div>
-    )
-}
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Logo;
